5e873324e979364cfda4eac7b5b6549f0c5df777
Karob2 <krichotomy@gmail.com>
2020-01-18 16:21:50 -0600

    dialog system complete

--- Source/ECS/Systems/PlayerControlSystem.cs
+++ Source/ECS/Systems/PlayerControlSystem.cs
@@ -18,48 +18,44 @@ namespace PovertySTG.ECS.Systems
 
         public override void Update(GameTime gameTime)
         {
-            DanmakuUpdate(gameTime);
-        }
+            sys.LevelScriptComponents.TryGetFirstEnabled(out LevelScriptComponent level);
 
-        void DanmakuUpdate(GameTime gameTime)
-        {
-            foreach (PlayerComponent component in sys.PlayerComponents.EnabledList)
+            PlayerComponent component = sys.PlayerComponents.First();
+            BodyComponent body = sys.BodyComponents.GetByOwner(component.Owner);
+            SpriteComponent sprite = sys.SpriteComponents.GetByOwner(component.Owner);
+
+            Vector2 d = new Vector2(0, 0);
+            if (InputManager.Held(GameCommand.Up)) d.Y -= 1;
+            if (InputManager.Held(GameCommand.Down)) d.Y += 1;
+            if (InputManager.Held(GameCommand.Left)) d.X -= 1;
+            if (InputManager.Held(GameCommand.Right)) d.X += 1;
+            if (d.Y != 0 || d.X != 0) d.Normalize();
+            if (InputManager.Held(GameCommand.Action3)) d *= 2f;
+            else d *= 6f;
+            body.DX = d.X;
+            body.DY = d.Y;
+            if (d.X < 0)
             {
-                BodyComponent body = sys.BodyComponents.GetByOwner(component.Owner);
-                Vector2 d = new Vector2(0, 0);
-                if (InputManager.Held(GameCommand.Up)) d.Y -= 1;
-                if (InputManager.Held(GameCommand.Down)) d.Y += 1;
-                if (InputManager.Held(GameCommand.Left)) d.X -= 1;
-                if (InputManager.Held(GameCommand.Right)) d.X += 1;
-                if (d.Y != 0 || d.X != 0) d.Normalize();
-                if (InputManager.Held(GameCommand.Action3)) d *= 2f;
-                else d *= 6f;
-                body.DX = d.X;
-                body.DY = d.Y;
-                SpriteComponent sprite = sys.SpriteComponents.GetByOwner(component.Owner);
-                if (d.X < 0)
-                {
-                    sprite.CurrentAnimation = "left";
-                }
-                else if (d.X > 0)
-                {
-                    sprite.CurrentAnimation = "right";
-                }
-                else
-                {
-                    sprite.CurrentAnimation = "still";
-                }
+                sprite.CurrentAnimation = "left";
+            }
+            else if (d.X > 0)
+            {
+                sprite.CurrentAnimation = "right";
+            }
+            else
+            {
+                sprite.CurrentAnimation = "still";
+            }
 
-                if (component.Timer > 0)
-                {
-                    component.Timer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
+            if (component.Timer > 0)
+            {
+                component.Timer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
 
-                if (InputManager.Held(GameCommand.Action1) && component.Timer <= 0)
-                {
-                    DanmakuFactory.MakeBullet(scene, 0, body.X, body.Y - 30, 0, -7);
-                    component.Timer = 0.16f;
-                }
+            if (InputManager.Held(GameCommand.Action1) && component.Timer <= 0 && level.Story == null)
+            {
+                DanmakuFactory.MakeBullet(scene, 0, body.X, body.Y - 30, 0, -7);
+                component.Timer = 0.16f;
             }
         }
     }
--- Source/ECS/Systems/StorySystem.cs
+++ Source/ECS/Systems/StorySystem.cs
@@ -1,4 +1,5 @@
 ﻿using Engine.ECS;
+using Engine.Input;
 using Engine.Resource;
 using Microsoft.Xna.Framework;
 using PovertySTG.ECS.Components;
@@ -22,14 +23,38 @@ namespace PovertySTG.ECS.Systems
             sys.LevelScriptComponents.TryGetFirstEnabled(out LevelScriptComponent level);
 
             if (talkBox == null) Bind();
-
             if (level.Story == null) return;
+
+            level.WaitTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (level.StoryProgress < 0)
             {
                 //foreach (Entity entity in scene.GetGroup("talk")) entity.Enable();
                 level.StoryProgress = 0;
+                level.WaitTimer = 0;
                 StoryLine line = level.Story.Lines[0];
                 ShowTalker1(line.Left, line.LeftMessage);
+                ShowTalker2(line.Right, line.RightMessage);
+                foreach (BulletComponent bullet in sys.BulletComponents.EnabledList)
+                {
+                    bullet.Owner.Disable();
+                }
+            }
+            else if (InputManager.JustPressed(GameCommand.Action1) && level.WaitTimer >= 0.5f)
+            {
+                level.StoryProgress++;
+                if (level.StoryProgress >= level.Story.Lines.Count)
+                {
+                    level.Story = null;
+                    level.WaitMode = LevelWaitMode.Start;
+                    ShowTalker1(null, null);
+                    ShowTalker2(null, null);
+                }
+                else
+                {
+                    StoryLine line = level.Story.Lines[level.StoryProgress];
+                    ShowTalker1(line.Left, line.LeftMessage);
+                    ShowTalker2(line.Right, line.RightMessage);
+                }
             }
         }
 
@@ -66,6 +91,32 @@ namespace PovertySTG.ECS.Systems
                 talkPortrait.Sprite = gs.ResourceManager.Sprites.Get("talk_" + speaker);
                 talkPortrait.CurrentAnimation = expression;
                 talkPortrait.Owner.Enable();
+            }
+        }
+
+        void ShowTalker2(string expression = null, string text = null)
+        {
+            if (text == null)
+            {
+                talkBox2.Disable();
+                talkText2.Owner.Disable();
+            }
+            else
+            {
+                talkText2.Text = text;
+                talkBox2.Enable();
+                talkText2.Owner.Enable();
+            }
+            if (expression == null)
+            {
+                talkPortrait2.Owner.Disable();
+            }
+            else
+            {
+                string speaker = expression.Split('_')[0];
+                talkPortrait2.Sprite = gs.ResourceManager.Sprites.Get("talk_" + speaker);
+                talkPortrait2.CurrentAnimation = expression;
+                talkPortrait2.Owner.Enable();
             }
         }
     }
